bdcae91d989ea543681830602547872844c9aca7
TKaburu <traceymwendwa@gmail.com>
2023-10-18 22:35:08 -0700

    Searches for the opecode

---
+++ exec.c
@@ -0,0 +1,45 @@
+#include "monty.h"
+
+
+/**
+ * get_opcode - This function checks the command
+ * @stack: point to the stack
+ * @line_number: Keep track of the line number
+ *
+ * Return: 0 on success or -1 if opecode not found
+ */
+
+int get_opcode(stack_t **stack, unsigned int line_number)
+{
+	int t = 0;
+	char *value = NULL, *comnd = NULL;
+
+	instruction_t file[] = {
+		{"push", pu_sh},
+		{"pall", pa_ll},
+		{"pint", pi_nt},
+		{"pop", po_p},
+		{NULL, NULL}
+	};
+
+	while (file[t].opcode)
+	{
+		if (strcmp(file[t].opcode, comnd) == 0)
+		{
+			if (strcmp(comnd, "push"))
+			{
+				if (isdigit(*value) == 1)
+					val = atoi(value);
+				else
+					return (1);
+			}
+			file[t].f(stack, line_number);
+			break;
+		}
+		t++;
+	}
+	if (!file[t].opcode)
+		return (-1);
+	return (0);
+}
+
