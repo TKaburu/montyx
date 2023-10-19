0e13385b8330fc7df24016b787394c5bb4477c86
TKaburu <traceymwendwa@gmail.com>
2023-10-19 16:50:24 -0700

    something went wrong

--- exec.c
+++ exec.c
@@ -1,6 +1,4 @@
 #include "monty.h"
-
-
 /**
  * get_opcode - This function checks the command
  * @stack: point to the stack
@@ -8,22 +6,18 @@
  *
  * Return: 0 on success or -1 if opecode not found
  */
-
 int get_opcode(stack_t **stack, unsigned int line_number)
 {
 	int t = 0;
 	char *value = NULL, *comnd = NULL;
-
 	instruction_t file[] = {
 		{"push", pu_sh},
 		{"pall", pa_ll},
 		{"pint", pi_nt},
 		{"pop", po_p},
-		{"swap", swa_p},
 		{"nop", nop},
 		{NULL, NULL}
 	};
-
 	while (file[t].opcode)
 	{
 		if (strcmp(file[t].opcode, comnd) == 0)
--- monty.h
+++ monty.h
@@ -1,19 +1,13 @@
 #ifndef MONTY_H
 #define MONTY_H
-
 #define _POSIX_C_SOURCE 200809L
-
 /* Standard headers used */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-
 extern int val;
-
 /* Structures */
-
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -29,7 +23,6 @@ typedef struct stack_s
 	struct stack_s *prev;
 	struct stack_s *next;
 } stack_t;
-
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
@@ -43,33 +36,22 @@ typedef struct instruction_s
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-
-
 /* run_monty Prototypes */
-
 int main(int argc, char **argv);
 int run_monty(int argc, char **argv);
-
 /* cmds prototypes */
-
 void pu_sh(stack_t **stack, unsigned int line_number);
 void pa_ll(stack_t **stack, unsigned int line_number);
 void pi_nt(stack_t **stack, unsigned int line_number);
 void po_p(stack_t **stack, unsigned int line_number);
-void swa_p(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 
 /* get opcode */
 
 int get_opcode(stack_t **stack, unsigned int line_number);
-
-
 /* Error handlers */
-
 void argument_error(void);
 void openf_error(void);
-
 /* Helper */
 stack_t *addnode(int data);
-
 #endif
